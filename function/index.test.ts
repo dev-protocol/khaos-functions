3e77621d25ba0e2342f4ca4ec3b8ee128b3f9704
aggre <hiroyuki.aggre@gmail.com>
2020-12-20 02:12:41 +0900

    update tests

--- function/index.test.ts
+++ function/index.test.ts
@@ -1,11 +1,70 @@
+/* eslint-disable functional/prefer-readonly-type */
+/* eslint-disable functional/no-let */
 import test from 'ava'
-import func from './index'
+import func, { CallingOptions } from './index'
+import * as importFunctions from './importFunctions'
+import * as callFunctions from './callFunctions'
+import { stub, SinonStub, spy, SinonSpy } from 'sinon'
+import * as example from '../functions/example'
 import { Context } from '@azure/functions'
+import { Functions } from '@devprotocol/khaos-core/types'
 
-test('Returns `Hello {query.name}` with 200', async (t) => {
-	const res = await func((undefined as unknown) as Context, {
-		query: { name: 'Alice' },
+let importFunctionsStub: SinonStub<
+	[id: string],
+	ReturnType<typeof importFunctions['importFunctions']>
+>
+let callFunctionsSpy: SinonSpy<
+	[Functions, CallingOptions],
+	ReturnType<typeof callFunctions['callFunctions']>
+>
+const importFunctionsSpy = spy(() => Promise.resolve(example))
+
+test.beforeEach(() => {
+	importFunctionsStub = stub(importFunctions, 'importFunctions').callsFake(
+		importFunctionsSpy
+	)
+	callFunctionsSpy = spy(callFunctions, 'callFunctions')
+})
+
+test.afterEach(() => {
+	importFunctionsStub.restore()
+	callFunctionsSpy.restore()
+})
+
+test.serial('Passes `id` in the request body to importFunctions', async (t) => {
+	const id = `${Math.random()}`
+	await func((undefined as unknown) as Context, {
+		body: { id },
 	})
-	t.is(res.body, 'Hello Alice')
-	t.is(res.status, 200)
+	t.deepEqual(importFunctionsSpy.getCall(0).args, [id])
+	t.is(importFunctionsStub.callCount, 1)
+})
+
+test.serial(
+	'Passes the result of importFunctions and the request body to callFunctions',
+	async (t) => {
+		const test = `${Math.random()}`
+		const expected = { id: 'example', test }
+		await func((undefined as unknown) as Context, {
+			body: expected,
+		})
+		t.deepEqual(callFunctionsSpy.getCall(0).args, [example, expected as any])
+		t.is(callFunctionsSpy.callCount, 1)
+	}
+)
+
+test.serial('Returns object for Azure Functions', async (t) => {
+	const res = await func((undefined as unknown) as Context, {
+		body: {
+			id: 'example',
+			method: 'addresses',
+			options: { network: 'mainnet' },
+		},
+	})
+	t.deepEqual(res, {
+		status: 200,
+		body: {
+			data: '0x0_mainnet',
+		},
+	})
 })
