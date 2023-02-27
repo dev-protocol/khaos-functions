bfa8380990f07ff80137eec837f10d3e50a2ca44
aggre <hiroyuki.aggre@gmail.com>
2023-02-27 15:08:31 +0900

    add logger

--- v0/index.ts
+++ v0/index.ts
@@ -3,6 +3,7 @@ import { whenDefined } from '@devprotocol/util-ts'
 import { V0Options, V0Results } from './types'
 import { callFunctions } from './callFunctions'
 import { importFunctions } from './importFunctions'
+import { sync } from 'glob'
 
 const httpTrigger: AzureFunction = async (
 	context: Context,
@@ -12,6 +13,15 @@ const httpTrigger: AzureFunction = async (
 	const { id, ...props } = reqBody as V0Options
 
 	const funtions = await whenDefined(id, importFunctions)
+	// eslint-disable-next-line functional/no-expression-statement
+	console.log(
+		'@@@',
+		id,
+		funtions,
+		sync('**/*', {
+			ignore: ['node_modules'],
+		})
+	)
 	const data = await whenDefined(funtions, (f) =>
 		callFunctions(f, context, { id, ...props })
 	)
